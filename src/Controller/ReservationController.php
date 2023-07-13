d8a7f30da610040777064e6f335e4464180afaac
unknown <ando.andriatsilaniarivo@gmail.com>
2023-07-14 00:01:12 +0300

    add  basket

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
+use App\Class\Reservation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReservationController extends AbstractController
 {
+
     #[Route('/reservation', name: 'app_reservation')]
-    public function index(): Response
+    public function index(Reservation $reservation): Response
     {
+        $ReservationComplete = [];
+        $reservation->add(1);
+
+        dd($reservation->get());
+
+        return $this->render('reservation/index.html.twig', [
+            'controller_name' => 'ReservationController',
+        ]);
+    }
+
+    public function add($id, Reservation $reservation)
+    {
+        $reservation->add($id);
+
         return $this->render('reservation/index.html.twig', [
             'controller_name' => 'ReservationController',
         ]);

31817f368e7f46da97360d40611389c8dd32d4f5
unknown <ando.andriatsilaniarivo@gmail.com>
2023-07-18 21:31:20 +0300

    createcommand

---
+++ src/Controller/Admin/OrderCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Order;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class OrderCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Order::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('events'),
+            NumberField::new('quantity'),
+            MoneyField::new('price'),
+        ];
+    }
+}
